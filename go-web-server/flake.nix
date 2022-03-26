13c52205924ad6df8618bbf41467034a2ceb770d
Xe <me@christine.website>
2022-03-26 18:19:36 +0000

    update for packages.default
    Signed-off-by: Xe <me@christine.website>

--- go-web-server/flake.nix
+++ go-web-server/flake.nix
@@ -17,31 +17,30 @@
         pkgs = nixpkgs.legacyPackages.${system};
         version = builtins.substring 0 8 self.lastModifiedDate;
       in {
-        defaultPackage = pkgs.buildGoModule {
-          pname = "web-server";
-          inherit version;
-          src = ./.;
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "web-server";
+            inherit version;
+            src = ./.;
 
-          # This hash locks the dependencies of this package. It is
-          # necessary because of how Go requires network access to resolve
-          # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-          # details. Normally one can build with a fake sha256 and rely on native Go
-          # mechanisms to tell you what the hash should be or determine what
-          # it should be "out-of-band" with other tooling (eg. gomod2nix).
-          # To begin with it is recommended to set this, but one must
-          # remeber to bump this hash when your dependencies change.
-          #vendorSha256 = pkgs.lib.fakeSha256;
-          vendorSha256 = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            # This hash locks the dependencies of this package. It is
+            # necessary because of how Go requires network access to resolve
+            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
+            # details. Normally one can build with a fake sha256 and rely on native Go
+            # mechanisms to tell you what the hash should be or determine what
+            # it should be "out-of-band" with other tooling (eg. gomod2nix).
+            # To begin with it is recommended to set this, but one must
+            # remeber to bump this hash when your dependencies change.
+            #vendorSha256 = pkgs.lib.fakeSha256;
+            vendorSha256 =
+              "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+          };
         };
 
-        packages = { };
+        defaultApp = utils.lib.mkApp { drv = self.packages.${system}.default; };
 
-        defaultApp = utils.lib.mkApp {
-          drv = self.defaultPackage.${system};
-        };
-
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ go gopls goimports go-tools ];
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ go gopls gotools go-tools ];
         };
       });
 }
